29b53add08f608b625c547de3b92e7b4730cfc9a
Prabj0t <prabpb31@gmail.com>
2024-04-11 00:02:18 +0530

    try many methods of creating Elements with in main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,10 +2,37 @@ import React from 'react'; // Importing React library
 import ReactDOM from 'react-dom/client'; // Importing ReactDOM library from client module
 import App from './App.jsx'; // Importing the App component from App.jsx file
 
+
+// const reactElement = {
+//   type: 'a', // Element type
+//   props: {
+//       href: 'https://google.com', // Props of the element
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google' // Children of the element
+// };
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+// using react
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com', // Props of the element
+    target: '_blank'
+  },
+  'Now with React Created Element'
+)
+
+
+
+
+
 // Rendering the App component wrapped in React.StrictMode
 // using ReactDOM.createRoot method
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App /> {/* Rendering the App component */}
-  </React.StrictMode>,
+  reactElement
 );
