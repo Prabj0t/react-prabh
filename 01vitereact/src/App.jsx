4b87f991df8456dfe5ef90e1c34129f45d0eeff1
Prabj0t <prabpb31@gmail.com>
2024-04-11 00:08:41 +0530

    curly brase integrated username

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,12 +2,17 @@ import Chai from './Prabh'; // Importing the chai component
 
 // Defining the App component
 function App() {
+
+  const username = "chai aur code"
   return (
     // JSX allows us to write HTML-like syntax in JavaScript
     // We can define our UI components using JSX and render them
     // JSX gets compiled to React.createElement calls by Babel
     // This JSX code renders an h1 element with the specified text
+    <>
     <Chai/>
+    <h1>chai pekr code kro with {username}</h1>
+    </>
   );
 }
 
