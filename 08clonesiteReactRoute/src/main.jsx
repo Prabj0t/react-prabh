b33c574768fc2370f0fff42a3f8061fde470a5c9
Prabj0t <prabpb31@gmail.com>
2024-04-21 23:48:06 +0530

    new project react route Dom

---
+++ 08clonesiteReactRoute/src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/home.jsx'
+import About from './components/About/About.jsx'
+// Creating Router
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Layout/>,
+    children : [
+      {
+        path: "",
+        element : <Home/>,
+      },
+      {
+        path: "about",
+        element : <About/>,
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
